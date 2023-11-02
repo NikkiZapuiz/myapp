0d6c665d874dfff75192a40b8ec8cacf3f9a1225
NikkiZapuiz <ma.nikki.zapuiz@gmail.com>
2023-11-02 21:37:49 +0800

    modified render-build.sh puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -11,12 +11,9 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-# Specifies that the worker count should equal the number of processors in production.
-workers ENV.fetch("WEB_CONCURRENCY") { 4 }
-
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+# worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT") { 3000 }
@@ -26,6 +23,11 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+# Specifies that the worker count should equal the number of processors in production.
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+
+preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
